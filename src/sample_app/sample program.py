5fb92c8ef597d119bd96613172f0fb7f2bca0f34
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-10-02 20:05:27 +0800

    Create sample program.py

---
+++ src/sample_app/sample program.py
@@ -0,0 +1,13 @@
+for i in range(0,min(len(df2.index), len(df1.index))):
+    try:
+        if df1.loc[i,'Column A'] != df2.loc[i,'Column A']:
+            if df2.loc[i,'Column A'] not in df1.loc[:,'Column A'].values:
+                difference_list2.append(df2.loc[i,'Column A'])
+                n = n + 1
+                df2.drop([i],inplace = True)
+            elif df1.loc[i,'Column A'] not in df2.loc[:,'Column A'].values:
+                difference_list1.append(df1.loc[i,'Column A'])
+                m = m + 1
+                df1.drop([i],inplace = True)
+    except (IndexError, KeyError) as error:
+        pass
