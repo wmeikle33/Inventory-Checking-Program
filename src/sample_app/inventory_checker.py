97e9a460f288f7eb73a726fd10702f7f67ce941d
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-29 23:02:02 +0800

    Update inventory_checker.py

--- src/sample_app/inventory_checker.py
+++ src/sample_app/inventory_checker.py
@@ -1,13 +1,15 @@
-for i in range(0,min(len(df2.index), len(df1.index))):
-    try:
-        if df1.loc[i,'Column A'] != df2.loc[i,'Column A']:
-            if df2.loc[i,'Column A'] not in df1.loc[:,'Column A'].values:
-                difference_list2.append(df2.loc[i,'Column A'])
-                n = n + 1
-                df2.drop([i],inplace = True)
-            elif df1.loc[i,'Column A'] not in df2.loc[:,'Column A'].values:
-                difference_list1.append(df1.loc[i,'Column A'])
-                m = m + 1
-                df1.drop([i],inplace = True)
-    except (IndexError, KeyError) as error:
-        pass
+
+def inventory_checker():
+    for i in range(0,min(len(df2.index), len(df1.index))):
+        try:
+            if df1.loc[i,'Column A'] != df2.loc[i,'Column A']:
+                if df2.loc[i,'Column A'] not in df1.loc[:,'Column A'].values:
+                    difference_list2.append(df2.loc[i,'Column A'])
+                    n = n + 1
+                    df2.drop([i],inplace = True)
+                elif df1.loc[i,'Column A'] not in df2.loc[:,'Column A'].values:
+                    difference_list1.append(df1.loc[i,'Column A'])
+                    m = m + 1
+                    df1.drop([i],inplace = True)
+        except (IndexError, KeyError) as error:
+            pass
